ae97a7e288d3afb23ad3eff1f8dd8d5c49a8bb68
Bettina Johnson <johnsonbettina17@gmail.com>
2020-06-25 03:04:59 +0000

    unable to use current code, but progress

--- word_frequency.py
+++ word_frequency.py
@@ -1,34 +1,71 @@
-STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
-]
+import re
+import string
+
+frequency = {}
+document_text = open('real_love.txt', 'r')
+text_string = document_text.read().lower()
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+ 
+for word in match_pattern:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+     
+frequency_list = frequency.keys()
+ 
+for words in frequency_list:
+    print (words, frequency[words])
+
+sort = sorted(count.items(), key=operator.itemgetter(1))
+sort.reverse
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    # Your code will go here. You can write additional functions if you want, and call them inside this function.
-    # first open the file
-    with open(file) as f:
-        lyrics = f.readlines()
-        word_list = [line.split() for line in lyrics]
-        print(word_list)
 
-# This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
-if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
+# STOP_WORDS = [
+#     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+#     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+#     'will', 'with'
+# ]
 
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
 
-    file = Path(args.file)
-    if file.is_file():
-        print_word_freq(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+# def print_word_freq(file):
+#     """Read in `file` and print out the frequency of words in that file."""
+#     # Your code will go here. You can write additional functions if you want, and call them inside this function.
+#     # first open the file
+#     with open(file) as f:
+#         lyrics = f.readlines()
+#         word_list = [line.split() for line in lyrics]
+#         print(word_list)
+
+# # This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
+# if __name__ == "__main__":
+#     import argparse
+#     from pathlib import Path
+
+#     parser = argparse.ArgumentParser(
+#         description='Get the word frequency in a text file.')
+#     parser.add_argument('file', help='file to read')
+#     args = parser.parse_args()
+
+#     file = Path(args.file)
+#     if file.is_file():
+#         print_word_freq(file)
+#     else:
+#         print(f"{file} does not exist!")
+#         exit(1)
+
+
+
+
+# stop_words = [
+#     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+#     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+#     'will', 'with'
+# ]
+
+
+
+
+
+
 
 
